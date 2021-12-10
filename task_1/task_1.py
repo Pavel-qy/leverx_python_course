981bbeb0f1b9f83a4120297d90d8f5e2c2e99e90
Pavel Stankevich <pavel.qy@gmail.com>
2021-12-10 13:50:58 +0300

    Refactor Task 1, refactor Task 2
    Task 1. Add using 'argparse' module, add new class and change the main
    logic. Improve the ability to scale.
    
    Task 2. Add using 'total_ordering' decorator. Cease to apply 're'
    module.

--- task_1/task_1.py
+++ task_1/task_1.py
@@ -1,46 +1,82 @@
+import argparse
 import json
+import os
 from dict2xml import dict2xml
 
 
-def open_json(path: str) -> list:
-    with open(path, "r") as file:
-        data = json.load(file)
-    return data
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script for combining two json files")
+    parser.add_argument(
+        "format",
+        help="Output format: 'json' or 'xml'",
+    )
+    parser.add_argument(
+        "--students",
+        default="data/students.json",
+        help="Path to 'students.json' file",
+        dest="students_path",
+    )
+    parser.add_argument(
+        "--rooms",
+        default="data/rooms.json",
+        help="Path to 'rooms.json' file",
+        dest="rooms_path",
+    )
+    args = parser.parse_args()
+    return args.format, args.students_path, args.rooms_path
 
 
-def combine_lists(students_list: list, rooms_list: list, dictionary_key: str) -> list:
-    for student in students_list:
-        try:
-            room_index = student["room"]
-            del student["room"]
-            rooms_list[room_index][dictionary_key].append(student)
-        except KeyError:
-            rooms_list[room_index][dictionary_key] = [student]
-    return rooms_list
+class Combining:
+    def __init__(self, format: str, students_path: str, rooms_path: str):
+        self.format = Combining.check_format(format)
+        self.students_path = Combining.check_file_path(students_path)
+        self.rooms_path = Combining.check_file_path(rooms_path)
+        self.students_list = Combining.read_json(self.students_path)
+        self.rooms_list = Combining.read_json(self.rooms_path)
 
+    @staticmethod
+    def check_format(format: str) -> str or ValueError:
+        supported_formats = ["json", "xml"]
+        if format.lower() in supported_formats:
+            return format.lower()
+        else:
+            raise ValueError(f"'{format} - unsupported format'")
+    
+    @staticmethod
+    def check_file_path(path: str) -> str or ValueError:
+        if os.path.isfile(path):
+            return path
+        else:
+            raise ValueError("invalid file path")
 
-def create_json(combined_list: list):
-    with open("rooms_with_students.json", "w") as file:
-        json.dump(combined_list, file, indent=4)
+    @staticmethod
+    def read_json(path) -> list:
+        with open(path, "r") as file:
+            data = json.load(file)
+        return data
 
+    def combine_lists(self) -> None:
+        dictionary_key = "student" if self.format == "xml" else "students"
+        for student in self.students_list:
+            try:
+                room_index = student["room"]
+                del student["room"]
+                self.rooms_list[room_index][dictionary_key].append(student)
+            except KeyError:
+                self.rooms_list[room_index][dictionary_key] = [student]
 
-def create_xml(combined_list: list):
-    with open("rooms_with_students.xml", "w") as file:
-        file.write(dict2xml(combined_list, wrap="room"))
+    def create_file(self) -> None:
+        with open(f"rooms_with_students.{self.format}", "w") as file:
+            if self.format == "json":
+                json.dump(self.rooms_list, file, indent=4)
+            elif self.format == "xml":
+                file.write(dict2xml(self.rooms_list, wrap="room"))
 
 
 def main():
-    students_list = open_json(input("Enter a relative path to a 'students' json file: "))
-    rooms_list = open_json(input("Enter a relative path to a 'rooms' json file: "))
-    format = input("Specify the output format, 'json' or 'xml': ").lower()
-    if format == "json":
-        dictionary_key = "students"
-        combined_list = combine_lists(students_list, rooms_list, dictionary_key)
-        create_json(combined_list)
-    elif format == "xml":
-        dictionary_key = "student"
-        combined_list = combine_lists(students_list, rooms_list, dictionary_key)
-        create_xml(combined_list)
+    combined = Combining(*parse_arguments())
+    combined.combine_lists()
+    combined.create_file()
 
 
 if __name__ == "__main__":
--- task_2/task_2.py
+++ task_2/task_2.py
@@ -1,64 +1,29 @@
 from itertools import zip_longest
-import re
+from functools import total_ordering
 
 
+@total_ordering
 class Version:
+    maturity_options = {
+        0.25: ("sr", "service-release", "service_release"),
+        -0.25: ("rc", "release-candidate", "release_candidate"),
+        -0.5: ("b", "beta"),
+        -0.75: ("a", "alpha"),
+    }
+
     def __init__(self, version):
         self.version = version
+        self.scraped_version = Version.scrape_numbers(self.version)
 
     def __eq__(self, other):
-        version_self = Version.scrape_numbers(self.version)
-        version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result == 0:
-            return True
-        else:
-            return False
-
-    def __ne__(self, other):
-        version_self = Version.scrape_numbers(self.version)
-        version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result != 0:
-            return True
-        else:
-            return False
+        other_scraped_version = Version.scrape_numbers(other.version)
+        result = Version.compare_version_lists(self.scraped_version, other_scraped_version)
+        return True if result == 0 else False
 
     def __lt__(self, other):
-        version_self = Version.scrape_numbers(self.version)
         version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result < 0:
-            return True
-        else:
-            return False
-
-    def __gt__(self, other):
-        version_self = Version.scrape_numbers(self.version)
-        version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result > 0:
-            return True
-        else:
-            return False
-
-    def __le__(self, other):
-        version_self = Version.scrape_numbers(self.version)
-        version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result <= 0:
-            return True
-        else:
-            return False
-
-    def __ge__(self, other):
-        version_self = Version.scrape_numbers(self.version)
-        version_other = Version.scrape_numbers(other.version)
-        result = Version.compare_version_lists(version_self, version_other)
-        if result >= 0:
-            return True
-        else:
-            return False
+        result = Version.compare_version_lists(self.scraped_version, version_other)
+        return True if result < 0 else False
 
     @staticmethod
     def scrape_numbers(version: str) -> list:
@@ -66,16 +31,68 @@ class Version:
         Scrape off the value of each rank of the version
 
         The string is split into ranks by points. Reduce each rank to the number and
-        add it to the list. If 'ValueError' raise try to scrape off the number 
-        at the beginning of the rank. If the number is missing assign '0'. 
-        From the same rank scrape off the name of the version maturity. 
-        Than substract the corresponding value: 'rc' - 0.25; 'b' - 0.5; 'a' - 0.75.
-        Add the resulting number to the list.
+        add it to the list. If 'ValueError' raise try to scrape off the number and
+        the name of the version maturity. Add the resulting number to the list.
 
         Parameters
         ----------
         version : str
             dot-separated values with maturity
+
+        Returns
+        -------
+        number_list : list
+            list of numbers corresponding to each rank of the version
+        """
+        number_list = []
+        for rank in version.split("."):
+            try:
+                number_list.append(int(rank))
+            except ValueError:
+                number = Version.scrape_maturity(rank)
+                number_list.append(number)
+        return number_list
+
+    @staticmethod
+    def scrape_maturity(rank: str) -> int:
+        """
+        Scrape off the name of the maturity of the rank
+
+        Itarate over characters and add them to 'raw_number' if it is a number,
+        or to 'maturity' if it is a letter. The resulting number is reduced to 'int'.
+
+        Parameters
+        ----------
+        rank : str
+            rank of the version
+        
+        Returns
+        -------
+        number : int
+            version rank value with maturity
+        """
+        raw_number = ""
+        maturity = ""
+        for c in rank:
+            if c.isdigit():
+                raw_number += c
+            elif c.isalpha():
+                maturity += c
+        raw_number = int(raw_number) if raw_number else 0
+        number = Version.apply_maturity(raw_number, maturity)
+        return number
+        
+    @staticmethod
+    def apply_maturity(raw_number: int, maturity: str) -> int:
+        """
+        Add the value corresponding to each maturity to the version rank     
+        
+        Parameters
+        ----------
+        raw_number : int
+            the number of the corresponding version rank
+        maturity : str
+            the maturity of the corresponding version rank
 
         Raises
         ------
@@ -84,29 +101,15 @@ class Version:
 
         Returns
         -------
-        number_list : list
-            list of numbers corresponding to each rank of the version
+        raw_number : int
+            the sum of the rank value and the corresponding maturity value
         """
-        number_list = []
-        for number in version.split("."):
-            try:
-                number_list.append(int(number))
-            except ValueError:
-                try:
-                    raw_number = int(re.findall(r"\b\d+", number)[0])
-                except IndexError:
-                    raw_number = 0
-                maturity = re.findall(r"[A-z]+", number)[0].lower()
-                if maturity in ("rc", "release-candidate", "release_candidate"):
-                    raw_number -= 0.25
-                elif maturity in ("b", "beta"):
-                    raw_number -= 0.5
-                elif maturity in ("a", "alpha"):
-                    raw_number -= 0.75
-                else:
-                    raise ValueError("failed to process value")
-                number_list.append(raw_number)
-        return number_list
+        for value, maturity_names in Version.maturity_options.items():
+            if maturity in maturity_names:
+                raw_number += value
+                return raw_number
+        else:
+            raise ValueError("failed to process maturity")
 
     @staticmethod
     def compare_version_lists(version_self: list, version_other: list) -> int:
@@ -123,7 +126,7 @@ class Version:
         version_self : str
             list of numbers of the first version
         version_other : str
-            list of numbers of the secon version
+            list of numbers of the second version
 
         Returns
         -------
